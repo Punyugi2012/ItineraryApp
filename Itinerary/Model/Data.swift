abac4557c147bf8d5dd841c14ab1e545326e3b80
Punyugi2012 <58160216@go.buu.ac.th>
2018-07-04 15:54:26 +0700

    Added readTrips code.

---
+++ Itinerary/Model/Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  Itinerary
+//
+//  Created by punyawee  on 4/7/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    static var tripModels = [TripModel]()
+}
