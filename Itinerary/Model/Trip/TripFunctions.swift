e634d62b3240f7a79e05e138b5cf2497113f5e9d
Punyugi2012 <58160216@go.buu.ac.th>
2018-07-04 15:46:19 +0700

    Added model and function trips

---
+++ Itinerary/Model/Trip/TripFunctions.swift
@@ -0,0 +1,25 @@
+//
+//  TripFunction.swift
+//  Itinerary
+//
+//  Created by punyawee  on 4/7/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import Foundation
+
+
+class TripFunction {
+    static func createTrip(tripModel: TripModel) {
+        
+    }
+    static func readTrips() {
+        
+    }
+    static func updateTrip(tripModel: TripModel) {
+        
+    }
+    static func deleteTrip(tripModel: TripModel) {
+        
+    }
+}
---
+++ Itinerary/Model/Trip/TripModel.swift
@@ -0,0 +1,19 @@
+//
+//  TripModel.swift
+//  Itinerary
+//
+//  Created by punyawee  on 4/7/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import Foundation
+
+class TripModel {
+    var id: String!
+    var title: String!
+    
+    init(title: String) {
+        id = UUID().uuidString
+        self.title = title
+    }
+}
