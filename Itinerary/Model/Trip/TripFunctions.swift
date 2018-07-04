5ce351e71d766779820b9b9ce0e0791574f63720
Punyugi2012 <58160216@go.buu.ac.th>
2018-07-04 16:07:20 +0700

    Run data 	retrieval on background thread.

--- Itinerary/Model/Trip/TripFunctions.swift
+++ Itinerary/Model/Trip/TripFunctions.swift
@@ -13,11 +13,16 @@ class TripFunction {
     static func createTrip(tripModel: TripModel) {
         
     }
-    static func readTrips() {
-        if Data.tripModels.count == 0{
-            Data.tripModels.append(TripModel(title: "Trip to Bali"))
-            Data.tripModels.append(TripModel(title: "Mexico"))
-            Data.tripModels.append(TripModel(title: "Russian Trip"))
+    static func readTrips(completion: @escaping () -> Void) {
+        DispatchQueue.global(qos: .userInteractive).async {
+            if Data.tripModels.count == 0 {
+                Data.tripModels.append(TripModel(title: "Trip to Bali"))
+                Data.tripModels.append(TripModel(title: "Mexico"))
+                Data.tripModels.append(TripModel(title: "Russian Trip"))
+            }
+        }
+        DispatchQueue.main.async {
+            completion()
         }
     }
     static func updateTrip(tripModel: TripModel) {
